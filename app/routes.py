0081fad5fb7b8713d8c5d3a4600ca72201273f60
Subhankar Nandy <shubhankarn300@gmail.com>
2026-07-26 16:08:43 +0530

    endpoint update

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
 from rag_chain import rag_chain
 from schema import ChatRequest
 
