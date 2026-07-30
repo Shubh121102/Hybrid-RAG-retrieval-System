ae5a376ef412931490126c973ceb05ac1b33e4d9
Subhankar Nandy <shubhankarn300@gmail.com>
2026-07-30 16:47:49 +0530

    cleaning up files and comments

--- rag_chain.py
+++ rag_chain.py
@@ -6,7 +6,7 @@ import os
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace 
 from ingestion import load_pdf, split_docs
 from retriever import generate_embeddings, create_vector_store, create_bm25_retriever, hybrid_retrieve
-# from langchain.chat_models import init_chat_model 
+
 
 load_dotenv()
 
@@ -16,22 +16,32 @@ if not os.environ.get("HF_TOKEN"):
 
 
 def rag_chain(question: str):
+    
+    # Defining the file path  
     file_path = "C:\\Users\\shubh\\OneDrive\\Desktop\\RAG\\data\\nke-10k-2023.pdf"
+
+    # Loading the Documents
     docs = load_pdf(file_path)
     split_documents = split_docs(docs)
-    # question = "How many distribution centres in the US?"
-    # question = input("Enter your question here:\n")
+
+    # Generating vector embeddings 
     embeddings = generate_embeddings()
+
+    # Creating the vector store
     vector_store = create_vector_store(embeddings, split_documents)
+
+    # Retriever for vector and BM25
     vector_retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 3})
     bm25_retriever = create_bm25_retriever(split_documents)
 
+
+    # Hybrid retriever  
     def hybrid_retrieve_fn(q):
         return hybrid_retrieve(query = q,retrievers=[vector_retriever,bm25_retriever],weights=[0.5,0.5])
 
     hybrid_retriever = RunnableLambda(hybrid_retrieve_fn)
 
-    
+    # Defining the LLM
     llm_endpoint = HuggingFaceEndpoint(
     repo_id="deepreinforce-ai/Ornith-1.0-9B", # Qwen/Qwen3-0.6B
     task="text-generation",
@@ -39,6 +49,8 @@ def rag_chain(question: str):
     do_sample=False,
 )
     llm = ChatHuggingFace(llm=llm_endpoint)
+
+    # Prompt Template
     prompt = ChatPromptTemplate.from_template(""" 
 Answer the questions based on the context below.
                                               
@@ -51,7 +63,9 @@ Make sure to answer in a concise manner, if you don't know the answer, just say 
 """)
     def format_docs(docs):
         return "\n".join([doc.page_content for doc in docs])
-    
+
+
+    # RAG chain 
     rag_chain = (
         {"context": hybrid_retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
@@ -59,7 +73,7 @@ Make sure to answer in a concise manner, if you don't know the answer, just say 
         | StrOutputParser()
     )
 
-    
+    # Final Output
     result = rag_chain.invoke(question)
     print("\n\nHYBRID RAG DEMO:\n")
     print(f"Q: {question}\n")
@@ -70,34 +84,3 @@ Make sure to answer in a concise manner, if you don't know the answer, just say 
 if __name__ == "__main__":
     rag_chain()
 
-
-
-
-#   # Create BM25 index
-#     bm25_index = create_bm25_retriever(split_documents)
-    
-#     # Define retriever functions
-#     def vector_retriever_func(query, top_k=10):
-#         return vector_store.similarity_search_with_score(query, k=top_k)
-    
-#     def bm25_wrapper(query, top_k=10):
-#         return bm25_retriever_func(query, bm25_index, split_documents, top_k)
-    
-#     # Create hybrid retriever function that returns documents (not just IDs)
-#     def hybrid_retriever_wrapper(query, top_k=10):
-#         # Get ranked results from hybrid_retriever
-#         ranked_results = hybrid_retriever(
-#             query=query,
-#             vector_retriever=vector_retriever_func,
-#             bm25_retriever=bm25_wrapper,
-#             rrf_constant=60,
-#             v_weights=0.5,
-#             b_weights=0.5,
-#             top_k=top_k
-#         )
-#         # Convert doc_id strings back to Document objects
-#         retrieved_docs = []
-#         for doc_id_str, score in ranked_results[:top_k]:
-#             doc_idx = int(doc_id_str)
-#             retrieved_docs.append(split_documents[doc_idx])
-#         return retrieved_docs
