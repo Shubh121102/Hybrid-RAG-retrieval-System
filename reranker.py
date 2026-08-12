2aa76460e0a1aab9caef43c5ad0852097217377e
Subhankar Nandy <shubhankarn300@gmail.com>
2026-08-13 00:05:52 +0530

    Reranker creation

--- reranker.py
+++ reranker.py
@@ -1,1 +1,10 @@
 #This is a simple reranker for reordering search results
+from sentence_transformers import CrossEncoder
+
+_reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+def rerank(query, docs, top_k=3):
+    pairs = [[query, doc.page_content] for doc in docs]
+    scores = _reranker.predict(pairs)
+    ranked = sorted(zip(scores, docs), key=lambda x: x[0], reverse=True)
+    return [doc for _, doc in ranked[:top_k]]
