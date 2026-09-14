679f7115028aacd1bfb1cf496a29c9b95595489b
Subhankar Nandy <shubhankarn300@gmail.com>
2026-09-14 14:51:52 +0530

    Creating new route for streaming

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
 import markdown
+import json
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
@@ -18,3 +19,23 @@ def chat(request: ChatRequest):
     result = rag_chain(request.question)
     return markdown.markdown(result)
     # return {"result": result}
+
+@router.post("/stream_answer", response_class = StreamingResponse)
+def stream_answer(request: ChatRequest):
+    def stream():
+
+        for chunk in rag_chain.stream(request.question):
+            if chunk:
+                yield json.dumps({
+                    "type": "answer_chunk",
+                    "content": chunk
+                }) + "\n"
+
+
+        yield json.dumps({
+            "type": "final_message",
+            "content": "Streaming Completed"
+        }) + "\n"
+
+
+    return StreamingResponse(stream(), media_type = "application/x-ndjson")
