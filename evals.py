07eb0a7a3a77fc88909fed09d549bc8e54d1112a
Subhankar Nandy <shubhankarn300@gmail.com>
2026-09-15 20:31:51 +0530

    Evals file updated with new embedding models and custom embedding function

--- evals.py
+++ evals.py
@@ -1,8 +1,9 @@
 from datasets import Dataset
 from ragas import evaluate
+from sentence_transformers import SentenceTransformer
 # from ragas.metrics.collections import faithfulness, answer_relevancy, context_precision, context_recall
 from ragas.metrics import Faithfulness, AnswerRelevancy, ContextPrecision, ContextRecall
-from ragas.embeddings import GoogleEmbeddings
+from ragas.embeddings import GoogleEmbeddings, HuggingFaceEmbeddings
 from ragas.llms import llm_factory
 from rag_chain import get_answer_and_contexts
 from google import genai
@@ -10,6 +11,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+class CustomEmbeddings:
+    def __init__(self, model_name="BAAI/bge-small-en-v1.5"):
+        self.model = SentenceTransformer(model_name)
+    
+    def embed_query(self, text: str):
+        """Embed a single query"""
+        return self.model.encode(text, normalize_embeddings=True).tolist()
+    
+    def embed_documents(self, texts: list):
+        """Embed multiple documents"""
+        return self.model.encode(texts, normalize_embeddings=True).tolist()
 
 client = genai.Client(api_key = os.environ.get("GOOGLE_API_KEY"))
 
@@ -40,12 +53,13 @@ for q, gt in zip(questions, ground_truths):
 dataset = Dataset.from_dict(data)
 
 llm = llm_factory(
-    "gemini-3.5-flash",
+    "gemini-3.5-flash-lite",
     provider = "google",
     client = client
 )
 
-embeddings = GoogleEmbeddings(client = client, model = "gemini-embedding-001")
+# embeddings = GoogleEmbeddings(client = client, model = "gemini-embedding-001")
+embeddings = CustomEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 
 metrics = [
     Faithfulness(llm = llm),
